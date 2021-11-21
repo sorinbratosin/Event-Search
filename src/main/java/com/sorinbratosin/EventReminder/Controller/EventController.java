ebc1b3be6a8e68fa6f38dd0e90498d1d84d5ec71
sorinbratosin <sorinbratosin93@yahoo.com>
2021-11-21 22:40:10 +0200

    Added the get all events as JSON in the dashboard url. Made the login and register GUI display in the center of the screen

--- src/main/java/com/sorinbratosin/EventReminder/Controller/EventController.java
+++ src/main/java/com/sorinbratosin/EventReminder/Controller/EventController.java
@@ -2,11 +2,14 @@ package com.sorinbratosin.EventReminder.Controller;
 
 
 import com.sorinbratosin.EventReminder.DAO.Event;
+import com.sorinbratosin.EventReminder.Security.UserSession;
 import com.sorinbratosin.EventReminder.Service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -16,10 +19,21 @@ public class EventController {
     @Autowired
     EventService eventService;
 
+    @Autowired
+    UserSession userSession;
+
 
     @GetMapping("/events")
     @ResponseBody
     public List<Event> eventList() {
         return eventService.findAll();
+    }
+
+    @GetMapping("/check")
+    public ModelAndView checkIfSessionIsActive() {
+        if(userSession.getUserId() == 0) {
+            return new ModelAndView("redirect:index.html");
+        }
+        return new ModelAndView("redirect:events");
     }
 }
